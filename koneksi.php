61bcf67a5bded26728a7e6e5fce09f1de57433eb
lita1246 <laaapejoy@gmail.com>
2026-05-20 22:45:29 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,1 @@
+<?php $host = "localhost"; $user = "root"; $pass = ""; $db = "ruangkita"; $conn = mysqli_connect($host, $user, $pass, $db); if (!$conn) { die("Koneksi database gagal: " . mysqli_connect_error()); } date_default_timezone_set('Asia/Jakarta'); ?>
