3ee3c501d14abe45bae60d9778ca0eb25712f1b1
Asyfahnabillah <m.fiqri6969@gmail.com>
2026-06-09 19:39:05 +0700

    kode sql

--- ruangkita.sql
+++ ruangkita.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2026 at 10:28 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 09 Jun 2026 pada 14.35
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookings`
+-- Struktur dari tabel `bookings`
 --
 
 CREATE TABLE `bookings` (
@@ -43,19 +43,22 @@ CREATE TABLE `bookings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data for table `bookings`
+-- Dumping data untuk tabel `bookings`
 --
 
-INSERT INTO `bookings` (`id`, `nama`, `prodi`, `email`, `checkin`, `checkout`, `created_at`, `angkatan`, `kelas`, `keperluan_booking`) VALUES
-(1, '', '', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2026-04-22 02:47:53', NULL, NULL, NULL),
-(2, 'uytr', 'ytre', 'elitasnm@gmail.com', '2026-04-23 09:00:00', '2026-04-23 18:00:00', '2026-04-22 02:52:30', NULL, NULL, NULL),
-(3, 'dea', 'psti', 'dea@gmail.com', '2026-04-22 10:00:00', '2026-04-22 18:00:00', '2026-04-22 03:02:36', NULL, NULL, NULL),
-(4, 'lit', 'psti', 'elitsnm1246@upi.edu', '2026-04-24 10:00:00', '2026-04-24 18:00:00', '2026-04-22 03:13:46', NULL, NULL, NULL);
+INSERT INTO `bookings` (`id`, `nama`, `prodi`, `email`, `ruangan_nama`, `checkin`, `checkout`, `created_at`, `angkatan`, `kelas`, `keperluan_booking`, `status`) VALUES
+(9, 'amirul', 'PSTI', 'am@gmail.com', 'RUANG B', '2026-05-01 01:00:00', '2026-05-01 02:00:00', '2026-05-07 08:57:35', '24', '4A', 'kelas', 'disetujui'),
+(10, 'Amirul Muhammad Rabbani', 'PSTI', 'amirulmr@student.upi.edu', 'RUANG A', '2026-06-07 08:06:00', '2026-06-07 17:16:00', '2026-05-28 13:04:19', '24', '4A', 'Biasaalah Anak Muda', 'disetujui'),
+(11, 'Amirul Muhammad Rabbani', 'PSTI', 'amirulmr@student.upi.edu', 'RUANG A', '2026-06-08 08:06:00', '2026-06-08 17:16:00', '2026-05-28 13:05:00', '24', '4A', 'Biasaalah Anak Muda', 'disetujui'),
+(12, 'Amiruo Muhammad Rabbani', 'PSTI', 'a@gmail.com', 'RUANG A', '2026-05-30 16:02:00', '2026-05-30 17:03:00', '2026-05-29 09:02:53', '24', '4A', 'Mau make buat dugem', 'disetujui'),
+(13, 'Amirul Muhammad Rabbani', 'PSTI', 'amirulmr@student.upi.edu', 'RUANG B', '2026-06-05 10:00:00', '2026-06-05 13:40:00', '2026-06-03 06:41:32', '24', '4A', 'Buat Cokber', 'pending'),
+(14, 'Amirul Muhammad Rabbani', 'PSTI', 'irgiebintang7@gmail.com', 'RUANG C', '2026-06-04 13:40:00', '2026-06-04 13:46:00', '2026-06-03 06:46:34', '24', '4A', 'Iclick', 'pending'),
+(15, 'ammirr', 'PSTI', 'a@gmail.com', 'RUANG B', '2026-06-04 13:49:00', '2026-06-04 13:50:00', '2026-06-03 06:50:01', '24', '4A', 'asdasd', 'pending');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ruangan`
+-- Struktur dari tabel `ruangan`
 --
 
 CREATE TABLE `ruangan` (
@@ -67,7 +70,7 @@ CREATE TABLE `ruangan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `ruangan`
+-- Dumping data untuk tabel `ruangan`
 --
 
 INSERT INTO `ruangan` (`id`, `nama`, `kapasitas`, `fasilitas`, `gambar`) VALUES
@@ -81,7 +84,7 @@ INSERT INTO `ruangan` (`id`, `nama`, `kapasitas`, `fasilitas`, `gambar`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -96,60 +99,63 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id_user`, `nama`, `email`, `password`, `jurusan`, `angkatan`, `kelas`) VALUES
-(1, 'zira', 'ra@gmail.com', '$2y$10$4Ktu3uDVTl8nlECuvPtK1.euFr/GqXfXWwVeS28HDOMDZjeXs0zjC', 'PSTI', '2024', '4A'),
-(2, 'dea', 'dea@gmail.com', '$2y$10$N47IgMEg8ll70iJuKrM.COcjvFyUFN6XArocrWROUMnymSPL.b8XO', 'MKB', '2024', '4B'),
-(3, 'lita', 'lita@gmail.com', '$2y$10$jwYRn2N.XTsbguYj8zmPa.U8gsnYLNj3F4vKBgX9LROvt0O4Ti3FK', 'SISTEL', '2023', '6A'),
-(4, 'amirul', 'amir@gmail.com', '$2y$10$ASecLXAaKNX1V35qHxUjy.Sk/VIddy2n4sH7UbGGUaVjyjqAFrzAK', 'PSTI', '2025', '2A'),
-(5, 'amir', 'amira@gmail.com', '$2y$10$8NkUfNCLF/y.dHPNl8PXu.zHCGyAVkHjVIUnfWeOPtMnVhLYOVevS', 'PSTI', '2025', '2A');
+INSERT INTO `users` (`id_user`, `nama`, `email`, `password`, `jurusan`, `angkatan`, `kelas`, `role`) VALUES
+(1, 'zira', 'ra@gmail.com', '$2y$10$4Ktu3uDVTl8nlECuvPtK1.euFr/GqXfXWwVeS28HDOMDZjeXs0zjC', 'PSTI', '2024', '4A', 'user'),
+(2, 'dea', 'dea@gmail.com', '$2y$10$N47IgMEg8ll70iJuKrM.COcjvFyUFN6XArocrWROUMnymSPL.b8XO', 'MKB', '2024', '4B', 'user'),
+(3, 'lita', 'lita@gmail.com', '$2y$10$jwYRn2N.XTsbguYj8zmPa.U8gsnYLNj3F4vKBgX9LROvt0O4Ti3FK', 'SISTEL', '2023', '6A', 'user'),
+(4, 'amirul', 'amir@gmail.com', '$2y$10$ASecLXAaKNX1V35qHxUjy.Sk/VIddy2n4sH7UbGGUaVjyjqAFrzAK', 'PSTI', '2025', '2A', 'user'),
+(5, 'amir', 'amira@gmail.com', '$2y$10$8NkUfNCLF/y.dHPNl8PXu.zHCGyAVkHjVIUnfWeOPtMnVhLYOVevS', 'PSTI', '2025', '2A', 'user'),
+(6, 'amirulmr', 'amirulmr@student.upi.edu', '$2y$10$3KzlDwO31.t7KEg.xHU07Ow9znp3iKPUQoTnCHAHZbRPRDeXBKV6S', 'PSTI', '2024', '4A', 'admin'),
+(7, 'aril', 'amirul@gmaill.com', '$2y$10$zn8mGmaCwdkuD6OTDXF6Iu9MUMLA3t4WNQd6ItDHERhmZhdOQ4Sye', 'PSTI', '2024', '4', 'user'),
+(8, 'ammirr', 'a@gmail.com', '$2y$10$R.YCFgkeZ0.iBFmXxJqDy.NiiUVprN/JP6MEzTBYH9e6o5/TeERNe', 'PSTI', '2024', '4', 'user');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `bookings`
+-- Indeks untuk tabel `bookings`
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `ruangan`
+-- Indeks untuk tabel `ruangan`
 --
 ALTER TABLE `ruangan`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `bookings`
+-- AUTO_INCREMENT untuk tabel `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
--- AUTO_INCREMENT for table `ruangan`
+-- AUTO_INCREMENT untuk tabel `ruangan`
 --
 ALTER TABLE `ruangan`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
